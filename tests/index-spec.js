e37d03114c591190afc75dac27dee0c7af9c862e
Alexis Corrieras <alexis.corrieras@gmail.com>
2019-05-15 11:21:25 -0400

    9

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -1,17 +1,22 @@
 // import { describe } from "mocha";
-import { describe } from "mocha";
-
-import {describe} from "mocha";
+import {
+  describe,
+} from 'mocha';
 
 const chai = require('chai');
-const expect = chai.expect;
+
+const {
+  expect,
+} = chai;
 const server = require('../server');
 
-console.log("test file, server = \n", server);
+// eslint-disable-next-line no-console
+console.log('test file, server = \n', server);
 
 describe('test', () => {
-    it('this simple test should return a string', () => {
-        expect('Test CI with Travis').to.equal('Test CI with Travis');
-    });
-    process.exit();
+  // eslint-disable-next-line no-undef
+  it('this simple test should return a string', () => {
+    expect('Test CI with Travis').to.equal('Test CI with Travis');
+  });
+  process.exit();
 });
